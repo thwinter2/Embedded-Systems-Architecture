368e4e2a6a0ef7d7a6213600d07e58b430064ae3
Alex Dean <agdean@ncsu.edu>
2019-11-18 18:14:28 -0500

    Initial commit

---
+++ Projects/Project_2/Project_2_Base/Source/LCD/colors.h
@@ -0,0 +1,23 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include "LCD.h"
+
+extern COLOR_T black, 
+	white,
+	red,
+	green,
+	blue,
+	yellow,
+	cyan,
+	magenta,
+	dark_red,
+	dark_green,
+	dark_blue,
+	dark_yellow,
+	dark_cyan,
+	dark_magenta,
+	orange,
+	light_gray, 
+	dark_gray;
+
+#endif // COLORS_H
